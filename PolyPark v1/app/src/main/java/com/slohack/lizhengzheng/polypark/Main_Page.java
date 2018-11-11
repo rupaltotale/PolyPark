223291abc54f6e4140f66c8b8de64b23ff5226a8
Zoe Li <zli32@calpoly.edu>
2018-11-10 20:39:19 -0800

    fixed page transition

--- PolyPark v1/app/src/main/java/com/slohack/lizhengzheng/polypark/Main_Page.java
+++ PolyPark v1/app/src/main/java/com/slohack/lizhengzheng/polypark/Main_Page.java
@@ -12,6 +12,7 @@ import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.Toast;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Main_Page extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
@@ -23,8 +24,9 @@ public class Main_Page extends AppCompatActivity implements AdapterView.OnItemSe
     EditText StartTime;
     EditText EndTime;
     Button submitButton;
-    Spinner dropdown2;
-    Spinner dropdown1;
+
+//    private final Spinner dropdown2;
+//    private final Spinner dropdown1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +34,14 @@ public class Main_Page extends AppCompatActivity implements AdapterView.OnItemSe
         setContentView(R.layout.activity_main__page);
 
         //time drop down menu
-        Spinner dropdown1 = findViewById(R.id.time);
+        final Spinner dropdown1 = findViewById(R.id.time);
         String[] items1 = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
         dropdown1.setAdapter(adapter1);
         dropdown1.setOnItemSelectedListener(this);
 
-
         //permit drop down menu
-        Spinner dropdown2 = findViewById(R.id.permit);
+        final Spinner dropdown2 = findViewById(R.id.permit);
         String[] items2 = new String[]{"N/A", "ABC", "XYZ", "EFG", "MNO", "PQR"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
         dropdown2.setAdapter(adapter2);
@@ -49,7 +50,42 @@ public class Main_Page extends AppCompatActivity implements AdapterView.OnItemSe
         StartTime = (EditText) findViewById(R.id.StartTime);
         EndTime = (EditText) findViewById(R.id.EndTime);
         submitButton = (Button) findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.print("did it run or no?");
+                String starTtime = StartTime.getText().toString();
+                String enEtime = EndTime.getText().toString();
+                starttime = Double.parseDouble(starTtime.substring(0, 2))+
+                        Double.parseDouble(starTtime.substring(3, 5))/60;
+                endtime =  Double.parseDouble(enEtime.substring(0, 2))/
+                        Double.parseDouble(enEtime.substring(3, 5))/60;
+
+                String mypermit = dropdown2.getSelectedItem().toString();
+                String mytime = dropdown1.getSelectedItem().toString();
+                Time start = new Time(starttime);
+                Time end = new Time(endtime);
+                boolean hasPermit = true;
+
+                if (mypermit == "N/A") { hasPermit = false;}
+
+                try {
+                    List<PermitLot> mylots = Fetcher.getParkingSpaces(mypermit, start, end, mytime, hasPermit);
+                    Intent intent = new Intent(Main_Page.this, AboutActivity.class);
+                    ArrayList <PermitLot> mylot = (ArrayList)mylots;
+                    intent.putExtra("parkinglist", mylot);
+                    startActivity(intent);
+                }
+
+                catch (Exception e) {}
+
+            }
+        });
+
     }
+
+
 
     private void showToast(String text) {
         Toast.makeText(Main_Page.this, text, Toast.LENGTH_SHORT).show();
@@ -70,31 +106,5 @@ public class Main_Page extends AppCompatActivity implements AdapterView.OnItemSe
         showToast(text);
     }
 
-
-    public void openAboutActivity(View v) {
-        String starTtime = StartTime.getText().toString();
-        String enEtime = EndTime.getText().toString();
-        starttime = Double.parseDouble(starTtime.substring(0, 2))+
-                Double.parseDouble(starTtime.substring(3, 5))/60;
-        endtime =  Double.parseDouble(enEtime.substring(0, 2))/
-                Double.parseDouble(enEtime.substring(3, 5))/60;
-
-        String mypermit = dropdown2.getSelectedItem().toString();
-        String mytime = dropdown1.getSelectedItem().toString();
-        Time start = new Time(starttime);
-        Time end = new Time(endtime);
-        boolean hasPermit = true;
-
-        if (mypermit == "N/A") { hasPermit = false;}
-
-        try {
-            List<PermitLot> mylots = Fetcher.getParkingSpaces(mypermit, start, end, mytime, hasPermit);
-        }
-        catch (Exception e) {}
-
-        Intent intent = new Intent(this, AboutActivity.class);
-        startActivity(intent);
-
-    }
 }
 
